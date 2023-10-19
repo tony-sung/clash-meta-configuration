19ce3f01d668cb026eac1475a3ca1d0f1512ce24
songtao1873 <479081675@qq.com>
2023-10-19 23:54:19 +0800

    update main

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import yaml
 from lxml import etree
 
 headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.46"
+    'Host': 'wanshanziwo.eu.org',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.46",
 }
 proxies = {'http': 'http://localhost:7890', 'https': 'http://localhost:7890'}
 url_list = {
@@ -18,14 +19,17 @@ url_list = {
         'xpath': '/html/body/div/section[1]/div/div/table/tbody/tr[2]/td[1]'
     }
 }
+
+
 def get_sublink_from_url(url, xpath):
     try:
         url_content = requests.get(url, headers=headers, proxies=proxies).text
         sub_link = etree.HTML(url_content).xpath(f'{xpath}/text()')[0]
-        return(sub_link)
+        return (sub_link)
     except Exception:
         print(f"get_sublink_from_url error: {url}\t{xpath}")
         return False
+
 # get sublink content
 def get_content_from_sublink(url):
     try:
@@ -33,14 +37,16 @@ def get_content_from_sublink(url):
         return sub_content
     except Exception:
         return False
+
 # get node from yaml
 def get_nodes_from_yaml(sub_content):
     try:
         nodes = yaml.safe_load(sub_content)['proxies']
-        return(nodes)
+        return (nodes)
     except Exception:
         print(f"get_nodes_from_yaml error: {sub_content}")
         return False
+
 # duplicates remove
 def duplicate_removel(node_content):
     node_content_duplicated = []
@@ -50,10 +56,13 @@ def duplicate_removel(node_content):
                 node_content_duplicated.append(node)
         node_content['proxies'] = node_content_duplicated
     return node_content
+
+
 if __name__ == "__main__":
     sub_content_list = []
     for name in url_list:
-        sub_link = get_sublink_from_url(url_list[name]['url'],url_list[name]['xpath'])
+        sub_link = get_sublink_from_url(
+            url_list[name]['url'], url_list[name]['xpath'])
         if sub_link:
             sub_content = get_content_from_sublink(sub_link)
         if sub_content:
